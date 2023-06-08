c2bcb97edf613833c6ab187f2c3869d29ed241ed
Le Anh Tuan <atctw.it@gmail.com>
2023-06-08 22:05:46 +0700

    update

--- src/pages/course/index.tsx
+++ src/pages/course/index.tsx
@@ -193,8 +193,6 @@ const MyCourses = () => {
                       </div>
                       <input
                         type="text"
-                        name="price"
-                        id="price"
                         className="block w-full rounded-md border-0 py-1.5 pl-10 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                         placeholder="Search course..."
                         value={searchText}
