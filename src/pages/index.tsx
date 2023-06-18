17b147a2878de5f4022e0f931e1e030f7cef57ec
Le Anh Tuan <atctw.it@gmail.com>
2023-06-18 15:19:22 +0700

    fix: remove unused component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import { Inter } from "next/font/google";
 import Layout from "@/components/layout";
 import Calendar from "@/components/calendar";
-import RecentCourses from "@/components/recent-courses";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <Layout>
-      {/* <RecentCourses /> */}
       <Calendar />
     </Layout>
   );
