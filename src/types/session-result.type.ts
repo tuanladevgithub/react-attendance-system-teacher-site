189e2785a2284928abd5876e6385f30860b3f885
Le Anh Tuan <atctw.it@gmail.com>
2023-07-02 16:23:10 +0700

    fix: fix session-result type

--- src/types/session-result.type.ts
+++ src/types/session-result.type.ts
@@ -1,3 +1,15 @@
+export type AttendanceStatus = {
+  id: number;
+
+  title: string;
+
+  acronym: string;
+
+  point: number;
+
+  auto_set_when_not_marked: number;
+};
+
 export type SessionResult = {
   t_attendance_session_id: number;
 
@@ -7,5 +19,9 @@ export type SessionResult = {
 
   record_time: string;
 
-  ip_address: string;
+  record_by_teacher: number; // 0 or 1
+
+  ip_address?: string;
+
+  attendanceStatus?: AttendanceStatus;
 };
