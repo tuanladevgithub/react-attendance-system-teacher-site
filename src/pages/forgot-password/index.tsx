54ae793631aa0074a9022a1448f067fe9a68efb2
Le Anh Tuan <atctw.it@gmail.com>
2023-08-03 22:32:11 +0700

    add: handle forgot password

--- src/pages/forgot-password/index.tsx
+++ src/pages/forgot-password/index.tsx
@@ -15,46 +15,40 @@
 
 import Image from "next/image";
 import logoImg from "../../../public/logo.svg";
-import Link from "next/link";
-import useUser from "@/lib/use-user";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import { useState } from "react";
 import axios from "axios";
-import Cookies from "js-cookie";
 import { ATTENDANCE_API_DOMAIN } from "@/constants/axios-constant";
 import spinnerImg from "../../../public/oval.svg";
 import { classNames } from "@/utils/class-name-util";
-import { delay } from "@/utils/delay-util";
+import { toast } from "react-toastify";
 
-const SignIn = () => {
-  const router = useRouter();
-  const { teacher, mutate, error } = useUser();
-  const [email, setEmail] = useState("");
-  const [pass, setPass] = useState("");
+const ForgotPassword = () => {
+  const [email, setEmail] = useState<string>();
   const [loading, setLoading] = useState(false);
   const [errorOccur, setErrorOccur] = useState(null);
 
-  useEffect(() => {
-    if (teacher && !error) {
-      router.replace("/");
-    }
-  }, [router, teacher, error]);
-
-  const loginTeacher = async (email: string, pass: string) => {
+  const handleSendResetCode = async () => {
     setLoading(true);
-    await delay(2000);
     try {
-      const { data } = await axios.post(
-        `${ATTENDANCE_API_DOMAIN}/auth/login-teacher`,
+      await axios.post(
+        `${ATTENDANCE_API_DOMAIN}/auth/send-reset-code-teacher`,
         {
           email,
-          pass,
         }
       );
 
-      Cookies.set("teacher_access_token", data.accessToken, {
-        expires: 365,
-      });
+      toast.info(
+        "You will receive a reset email if user with that email exits.",
+        {
+          position: "bottom-center",
+          autoClose: 4000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          theme: "light",
+        }
+      );
     } catch (error: any) {
       setErrorOccur(error.response.data.message);
     }
@@ -71,7 +65,7 @@ const SignIn = () => {
             alt="Logo HUST"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
+            Forgot password
           </h2>
         </div>
 
@@ -102,37 +96,6 @@ const SignIn = () => {
             </div>
 
             <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Password
-                </label>
-                <div className="text-sm">
-                  <Link
-                    href="/forgot-password"
-                    className="font-semibold text-indigo-600 hover:text-indigo-500"
-                  >
-                    Forgot password?
-                  </Link>
-                </div>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  placeholder="*********"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  onChange={(e) => setPass(e.target.value)}
-                />
-              </div>
-            </div>
-
-            <div>
               <button
                 type="button"
                 className={classNames(
@@ -142,10 +105,7 @@ const SignIn = () => {
                   "flex w-full justify-center items-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm",
                   "focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 )}
-                onClick={() => {
-                  loginTeacher(email, pass);
-                  mutate();
-                }}
+                onClick={handleSendResetCode}
               >
                 {loading && (
                   <div className="mr-2">
@@ -156,24 +116,14 @@ const SignIn = () => {
                     />
                   </div>
                 )}
-                <span>Sign in</span>
+                <span>Send Reset Code</span>
               </button>
             </div>
           </form>
-
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?
-            <Link
-              href="#"
-              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-            >
-              {" Please contact your admin!"}
-            </Link>
-          </p>
         </div>
       </div>
     </>
   );
 };
 
-export default SignIn;
+export default ForgotPassword;
--- src/pages/reset-password/[resetCode].tsx
+++ src/pages/reset-password/[resetCode].tsx
@@ -15,48 +15,48 @@
 
 import Image from "next/image";
 import logoImg from "../../../public/logo.svg";
-import Link from "next/link";
-import useUser from "@/lib/use-user";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
-import Cookies from "js-cookie";
 import { ATTENDANCE_API_DOMAIN } from "@/constants/axios-constant";
 import spinnerImg from "../../../public/oval.svg";
 import { classNames } from "@/utils/class-name-util";
-import { delay } from "@/utils/delay-util";
+import { toast } from "react-toastify";
 
-const SignIn = () => {
+const ResetPassword = () => {
   const router = useRouter();
-  const { teacher, mutate, error } = useUser();
-  const [email, setEmail] = useState("");
-  const [pass, setPass] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [errorOccur, setErrorOccur] = useState(null);
+  const [newPass, setNewPass] = useState<string>();
+  const [newPassConfirm, setNewPassConfirm] = useState<string>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [errorOccur, setErrorOccur] = useState<string>();
 
-  useEffect(() => {
-    if (teacher && !error) {
-      router.replace("/");
-    }
-  }, [router, teacher, error]);
-
-  const loginTeacher = async (email: string, pass: string) => {
+  const handleResetPassword = async () => {
     setLoading(true);
-    await delay(2000);
-    try {
-      const { data } = await axios.post(
-        `${ATTENDANCE_API_DOMAIN}/auth/login-teacher`,
-        {
-          email,
-          pass,
-        }
-      );
+    if (newPass && newPassConfirm && newPass === newPassConfirm) {
+      try {
+        await axios.patch(
+          `${ATTENDANCE_API_DOMAIN}/auth/reset-password-teacher/${router.query.resetCode}`,
+          {
+            newPass,
+          }
+        );
 
-      Cookies.set("teacher_access_token", data.accessToken, {
-        expires: 365,
-      });
-    } catch (error: any) {
-      setErrorOccur(error.response.data.message);
+        toast.success("Change password successfully!", {
+          position: "bottom-center",
+          autoClose: 4000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          theme: "light",
+        });
+
+        await router.push("/sign-in");
+      } catch (error: any) {
+        setErrorOccur(error.response.data.message);
+      }
+    } else {
+      setErrorOccur("Check your password and confirm");
     }
     setLoading(false);
   };
@@ -71,7 +71,7 @@ const SignIn = () => {
             alt="Logo HUST"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
+            Reset your password
           </h2>
         </div>
 
@@ -82,52 +82,40 @@ const SignIn = () => {
           <form className="space-y-6">
             <div>
               <label
-                htmlFor="email"
+                htmlFor="newPass"
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
-                Email address
+                New password
               </label>
               <div className="mt-2">
                 <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
+                  id="newPass"
+                  name="newPass"
+                  type="password"
                   required
-                  placeholder="sample@example.com"
+                  placeholder="*********"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  onChange={(e) => setEmail(e.target.value)}
+                  onChange={(e) => setNewPass(e.target.value)}
                 />
               </div>
             </div>
 
             <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Password
-                </label>
-                <div className="text-sm">
-                  <Link
-                    href="/forgot-password"
-                    className="font-semibold text-indigo-600 hover:text-indigo-500"
-                  >
-                    Forgot password?
-                  </Link>
-                </div>
-              </div>
+              <label
+                htmlFor="newPassConfirm"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Confirm password
+              </label>
               <div className="mt-2">
                 <input
-                  id="password"
-                  name="password"
+                  id="newPassConfirm"
+                  name="newPassConfirm"
                   type="password"
-                  autoComplete="current-password"
                   required
                   placeholder="*********"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  onChange={(e) => setPass(e.target.value)}
+                  onChange={(e) => setNewPassConfirm(e.target.value)}
                 />
               </div>
             </div>
@@ -142,10 +130,7 @@ const SignIn = () => {
                   "flex w-full justify-center items-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm",
                   "focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 )}
-                onClick={() => {
-                  loginTeacher(email, pass);
-                  mutate();
-                }}
+                onClick={handleResetPassword}
               >
                 {loading && (
                   <div className="mr-2">
@@ -156,24 +141,14 @@ const SignIn = () => {
                     />
                   </div>
                 )}
-                <span>Sign in</span>
+                <span>Reset Password</span>
               </button>
             </div>
           </form>
-
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?
-            <Link
-              href="#"
-              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-            >
-              {" Please contact your admin!"}
-            </Link>
-          </p>
         </div>
       </div>
     </>
   );
 };
 
-export default SignIn;
+export default ResetPassword;
